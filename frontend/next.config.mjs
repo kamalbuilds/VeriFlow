118e0bcd7bbfa02a36ff98ea4fdc2aa285f38b55
kamalbuilds <geniusamansingh@gmail.com>
2025-07-03 00:39:16 +0530

    add ts config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -8,6 +8,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   // swcMinify: false, // Disable SWC minification
   // experimental: {
   //   forceSwcTransforms: false, // Disable forced SWC transforms
